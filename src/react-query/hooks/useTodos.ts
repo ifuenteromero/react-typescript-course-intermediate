3eaa0e9c3cecc187a3ec44413502f157e3f9766e
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-01-08 18:24:55 +0100

    Creating a custom query hook

---
+++ src/react-query/hooks/useTodos.ts
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../../services/api-client';
+import endpoints from '../../services/endpoints';
+
+interface Todo {
+	id: number;
+	title: string;
+	userId: number;
+	completed: boolean;
+}
+
+const useTodos = () => {
+	const fetchTodos = () =>
+		apiClient.get<Todo[]>(endpoints.todos).then((res) => res.data);
+
+	const { data, error, isLoading } = useQuery<Todo[], Error>({
+		queryKey: ['todos'],
+		queryFn: fetchTodos,
+	});
+
+	return { data, error, isLoading };
+};
+
+export default useTodos;
