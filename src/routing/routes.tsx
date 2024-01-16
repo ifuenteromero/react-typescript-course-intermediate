9162568575f0ea93b6bef9bb0267dd1097314ba0
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-01-16 17:23:16 +0100

    Config env variables

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,15 +2,20 @@ import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './HomePage';
 import UserListPage from './UserListPage';
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <HomePage />,
-	},
-	{
-		path: '/users',
-		element: <UserListPage />,
-	},
-]);
+const baseUrl = import.meta.env.BASE_URL;
+
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <HomePage />,
+		},
+		{
+			path: '/users',
+			element: <UserListPage />,
+		},
+	],
+	{ basename: baseUrl }
+);
 
 export default router;
